ce07809bef6b4dd81fe046b1e528ab8ecaab8235
KittichoteKamalapirat <kittichoteshane@gmail.com>
2023-08-08 00:50:19 +0700

    fix footer

--- src/app/components/sections/FooterSection.tsx
+++ src/app/components/sections/FooterSection.tsx
@@ -1,18 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
+import SectionWrapper from "../wrappers/SectionWrapper";
 
 interface Props {}
 
 const FooterSection = ({}: Props) => {
   return (
-    <footer>
-      <div className="bg-primary-100 px-4 py-6 md:flex md:items-center md:justify-between w-screen text-center">
-        <div className="mx-auto w-full max-w-screen-xl">
-          <span className="text-sm sm:text-center">
-            Looking forward to meeting all the guests 👰🏻🤵🏻
-          </span>
+    <SectionWrapper className="relative w-screen py-8 md:py-8 bg-primary-100 flex items-center justify-center">
+      <footer>
+        <div className="px-4 py-6 md:flex md:items-center md:justify-between w-screen text-center">
+          <div className="mx-auto w-full max-w-screen-xl">
+            <h2 className="text-lg md:text-2xl text-center my">
+              Looking forward to meeting all the guests 👰🏻🤵🏻
+            </h2>
+            <img
+              alt="Footer"
+              src={`/images/looking-forward.webp`}
+              className="w-11/12 md:max-w-[300px] mx-auto mt-10"
+            />
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </SectionWrapper>
   );
 };
 export default FooterSection;
